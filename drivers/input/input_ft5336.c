4c6194207cb4bd40e8fb41b38705fe2db12cedf8
coskunergan <coskun.ergan@gtmeu.com.tr>
2023-12-29 12:05:00 +0300

    -touch configure accor. to st1633i

--- drivers/input/input_ft5336.c
+++ drivers/input/input_ft5336.c
@@ -17,7 +17,7 @@ LOG_MODULE_REGISTER(ft5336, CONFIG_INPUT_LOG_LEVEL);
 
 /* FT5336 used registers */
 #define REG_TD_STATUS		0x02U
-#define REG_P1_XH		0x03U
+#define REG_P1_XH		0x10U
 
 /* REG_TD_STATUS: Touch points. */
 #define TOUCH_POINTS_POS	0U
@@ -36,189 +36,216 @@ LOG_MODULE_REGISTER(ft5336, CONFIG_INPUT_LOG_LEVEL);
 #define POSITION_H_MSK		0x0FU
 
 /** FT5336 configuration (DT). */
-struct ft5336_config {
-	/** I2C bus. */
-	struct i2c_dt_spec bus;
-	struct gpio_dt_spec reset_gpio;
+struct ft5336_config
+{
+    /** I2C bus. */
+    struct i2c_dt_spec bus;
+    struct gpio_dt_spec reset_gpio;
 #ifdef CONFIG_INPUT_FT5336_INTERRUPT
-	/** Interrupt GPIO information. */
-	struct gpio_dt_spec int_gpio;
+    /** Interrupt GPIO information. */
+    struct gpio_dt_spec int_gpio;
 #endif
 };
 
 /** FT5336 data. */
-struct ft5336_data {
-	/** Device pointer. */
-	const struct device *dev;
-	/** Work queue (for deferred read). */
-	struct k_work work;
+struct ft5336_data
+{
+    /** Device pointer. */
+    const struct device *dev;
+    /** Work queue (for deferred read). */
+    struct k_work work;
 #ifdef CONFIG_INPUT_FT5336_INTERRUPT
-	/** Interrupt GPIO callback. */
-	struct gpio_callback int_gpio_cb;
+    /** Interrupt GPIO callback. */
+    struct gpio_callback int_gpio_cb;
 #else
-	/** Timer (polling mode). */
-	struct k_timer timer;
+    /** Timer (polling mode). */
+    struct k_timer timer;
 #endif
-	/** Last pressed state. */
-	bool pressed_old;
+    /** Last pressed state. */
+    bool pressed_old;
 };
+
+typedef struct
+{
+    uint8_t y_h: 3,
+            reserved: 1,
+            x_h: 3,
+            valid: 1;
+    uint8_t x_l;
+    uint8_t y_l;
+    uint8_t z;
+} xyz_data_t;
+typedef struct
+{
+    uint8_t fingers: 4,
+            reserved: 4;
+    uint8_t keys;
+    xyz_data_t xyz_data[10];
+} stx_report_data_t;
 
 static int ft5336_process(const struct device *dev)
 {
-	const struct ft5336_config *config = dev->config;
-	struct ft5336_data *data = dev->data;
+    const struct ft5336_config *config = dev->config;
+    struct ft5336_data *data = dev->data;
 
-	int r;
-	uint8_t points;
-	uint8_t coords[9U];
-	uint8_t event;
-	uint16_t row, col;
-	bool pressed;
+    int r;
+    uint8_t points;
+    uint8_t coords[9U];
+    uint8_t event;
+    uint16_t row, col;
+    bool pressed;
 
-	// /* obtain number of touch points (NOTE: multi-touch ignored) */
-	// r = i2c_reg_read_byte_dt(&config->bus, REG_TD_STATUS, &points);
-	// if (r < 0) {
-		//return r;
-	// }
-	
-	r = i2c_burst_read_dt(&config->bus, REG_P1_XH, coords, sizeof(coords));
-	if (r < 0) {
-		return r;
-	}		
-	
-	pressed = (coords[4] == 0x5A && coords[5] != 0xFF && coords[6] != 0xFF && coords[7] != 0xFF);
-	
-	
-	col = (uint16_t)((uint16_t)((uint16_t)coords[5] & 0x70) << 4) | coords[6];
-	row = (uint16_t)((uint16_t)((uint16_t)coords[5] & 0x7) << 8) | coords[7];
-		
-	row = (row * 480UL) / 2047UL;	
-	col = (col * 800UL) / 2047UL;
+    r = i2c_burst_read_dt(&config->bus, REG_P1_XH, coords, sizeof(coords));
+    if(r < 0)
+    {
+        return r;
+    }
 
-	//points = (points >> TOUCH_POINTS_POS) & TOUCH_POINTS_MSK;
-	//if (points != 0U && points != 1U) {
-	//	return 0;
-	//}
+    pressed = false;
 
-	/* obtain first point X, Y coordinates and event from:
-	 * REG_P1_XH, REG_P1_XL, REG_P1_YH, REG_P1_YL.
-	 */
-	// r = i2c_burst_read_dt(&config->bus, REG_P1_XH, coords, sizeof(coords));
-	// if (r < 0) {
-		// return r;
-	// }
+    uint16_t x0, y0;
+	//uint16_t x1, y1;
 
-	// event = (coords[0] >> EVENT_POS) & EVENT_MSK;
-	// row = ((coords[0] & POSITION_H_MSK) << 8U) | coords[1];
-	// col = ((coords[2] & POSITION_H_MSK) << 8U) | coords[3];
-	// pressed = (event == EVENT_PRESS_DOWN) || (event == EVENT_CONTACT);
+    stx_report_data_t *pdata;
 
-	//LOG_DBG("event: %d, row: %d, col: %d", event, row, col);
+    pdata = (stx_report_data_t *) coords;
 
-	if (pressed) {
-		input_report_abs(dev, INPUT_ABS_X, col, false, K_FOREVER);
-		input_report_abs(dev, INPUT_ABS_Y, row, false, K_FOREVER);
-		input_report_key(dev, INPUT_BTN_TOUCH, 1, true, K_FOREVER);
-	} else if (data->pressed_old && !pressed) {
-		input_report_key(dev, INPUT_BTN_TOUCH, 0, true, K_FOREVER);
-	}
-	data->pressed_old = pressed;
+    if(pdata->xyz_data[0].valid)
+    {
+        pressed = true;
+        x0 = pdata->xyz_data[0].x_h << 8 | pdata->xyz_data[0].x_l;
+        y0 = pdata->xyz_data[0].y_h << 8 | pdata->xyz_data[0].y_l;
+    }
+    // if(pdata->xyz_data[1].valid)
+    // {
+    //     pressed = true;
+    //     x1 = pdata->xyz_data[1].x_h << 8 | pdata->xyz_data[1].x_l;
+    //     y1 = pdata->xyz_data[1].y_h << 8 | pdata->xyz_data[1].y_l;
+    // }
 
-	return 0;
+    col = x0;
+    row = y0;
+
+    col = 800 - col;
+    row = 480 - row;
+
+    LOG_DBG("event: %d, row: %d, col: %d", event, row, col);
+
+    if(pressed)
+    {
+        input_report_abs(dev, INPUT_ABS_X, col, false, K_FOREVER);
+        input_report_abs(dev, INPUT_ABS_Y, row, false, K_FOREVER);
+        input_report_key(dev, INPUT_BTN_TOUCH, 1, true, K_FOREVER);
+    }
+    else if(data->pressed_old && !pressed)
+    {
+        input_report_key(dev, INPUT_BTN_TOUCH, 0, true, K_FOREVER);
+    }
+    data->pressed_old = pressed;
+
+    return 0;
 }
 
 static void ft5336_work_handler(struct k_work *work)
 {
-	struct ft5336_data *data = CONTAINER_OF(work, struct ft5336_data, work);
+    struct ft5336_data *data = CONTAINER_OF(work, struct ft5336_data, work);
 
-	ft5336_process(data->dev);
+    ft5336_process(data->dev);
 }
 
 #ifdef CONFIG_INPUT_FT5336_INTERRUPT
 static void ft5336_isr_handler(const struct device *dev,
-			       struct gpio_callback *cb, uint32_t pins)
+                               struct gpio_callback *cb, uint32_t pins)
 {
-	struct ft5336_data *data = CONTAINER_OF(cb, struct ft5336_data, int_gpio_cb);
+    struct ft5336_data *data = CONTAINER_OF(cb, struct ft5336_data, int_gpio_cb);
 
-	k_work_submit(&data->work);
+    k_work_submit(&data->work);
 }
 #else
 static void ft5336_timer_handler(struct k_timer *timer)
 {
-	struct ft5336_data *data = CONTAINER_OF(timer, struct ft5336_data, timer);
+    struct ft5336_data *data = CONTAINER_OF(timer, struct ft5336_data, timer);
 
-	k_work_submit(&data->work);
+    k_work_submit(&data->work);
 }
 #endif
 
 static int ft5336_init(const struct device *dev)
 {
-	const struct ft5336_config *config = dev->config;
-	struct ft5336_data *data = dev->data;
-	int r;
+    const struct ft5336_config *config = dev->config;
+    struct ft5336_data *data = dev->data;
+    int r;
 
-	if (!device_is_ready(config->bus.bus)) {
-		LOG_ERR("I2C controller device not ready");
-		return -ENODEV;
-	}
+    if(!device_is_ready(config->bus.bus))
+    {
+        LOG_ERR("I2C controller device not ready");
+        return -ENODEV;
+    }
 
-	data->dev = dev;
+    data->dev = dev;
 
-	k_work_init(&data->work, ft5336_work_handler);
+    k_work_init(&data->work, ft5336_work_handler);
 
-	if (config->reset_gpio.port != NULL) {
-		/* Enable reset GPIO, and pull down */
-		r = gpio_pin_configure_dt(&config->reset_gpio, GPIO_OUTPUT_INACTIVE);
-		if (r < 0) {
-			LOG_ERR("Could not enable reset GPIO");
-			return r;
-		}
-		/*
-		 * Datasheet requires reset be held low 1 ms, or
-		 * 1 ms + 100us if powering on controller. Hold low for
-		 * 5 ms to be safe.
-		 */
-		k_sleep(K_MSEC(5));
-		/* Pull reset pin high to complete reset sequence */
-		r = gpio_pin_set_dt(&config->reset_gpio, 1);
-		if (r < 0) {
-			return r;
-		}
-	}
+    if(config->reset_gpio.port != NULL)
+    {
+        /* Enable reset GPIO, and pull down */
+        r = gpio_pin_configure_dt(&config->reset_gpio, GPIO_OUTPUT_INACTIVE);
+        if(r < 0)
+        {
+            LOG_ERR("Could not enable reset GPIO");
+            return r;
+        }
+        /*
+         * Datasheet requires reset be held low 1 ms, or
+         * 1 ms + 100us if powering on controller. Hold low for
+         * 5 ms to be safe.
+         */
+        k_sleep(K_MSEC(5));
+        /* Pull reset pin high to complete reset sequence */
+        r = gpio_pin_set_dt(&config->reset_gpio, 1);
+        if(r < 0)
+        {
+            return r;
+        }
+    }
 
 #ifdef CONFIG_INPUT_FT5336_INTERRUPT
 
-	if (!gpio_is_ready_dt(&config->int_gpio)) {
-		LOG_ERR("Interrupt GPIO controller device not ready");
-		return -ENODEV;
-	}
+    if(!gpio_is_ready_dt(&config->int_gpio))
+    {
+        LOG_ERR("Interrupt GPIO controller device not ready");
+        return -ENODEV;
+    }
 
-	r = gpio_pin_configure_dt(&config->int_gpio, GPIO_INPUT);
-	if (r < 0) {
-		LOG_ERR("Could not configure interrupt GPIO pin");
-		return r;
-	}
+    r = gpio_pin_configure_dt(&config->int_gpio, GPIO_INPUT);
+    if(r < 0)
+    {
+        LOG_ERR("Could not configure interrupt GPIO pin");
+        return r;
+    }
 
-	r = gpio_pin_interrupt_configure_dt(&config->int_gpio,
-					    GPIO_INT_EDGE_TO_ACTIVE);
-	if (r < 0) {
-		LOG_ERR("Could not configure interrupt GPIO interrupt.");
-		return r;
-	}
+    r = gpio_pin_interrupt_configure_dt(&config->int_gpio,
+                                        GPIO_INT_EDGE_TO_ACTIVE);
+    if(r < 0)
+    {
+        LOG_ERR("Could not configure interrupt GPIO interrupt.");
+        return r;
+    }
 
-	gpio_init_callback(&data->int_gpio_cb, ft5336_isr_handler,
-			   BIT(config->int_gpio.pin));
-	r = gpio_add_callback(config->int_gpio.port, &data->int_gpio_cb);
-	if (r < 0) {
-		LOG_ERR("Could not set gpio callback");
-		return r;
-	}
+    gpio_init_callback(&data->int_gpio_cb, ft5336_isr_handler,
+                       BIT(config->int_gpio.pin));
+    r = gpio_add_callback(config->int_gpio.port, &data->int_gpio_cb);
+    if(r < 0)
+    {
+        LOG_ERR("Could not set gpio callback");
+        return r;
+    }
 #else
-	k_timer_init(&data->timer, ft5336_timer_handler, NULL);
-	k_timer_start(&data->timer, K_MSEC(CONFIG_INPUT_FT5336_PERIOD),
-		      K_MSEC(CONFIG_INPUT_FT5336_PERIOD));
+    k_timer_init(&data->timer, ft5336_timer_handler, NULL);
+    k_timer_start(&data->timer, K_MSEC(CONFIG_INPUT_FT5336_PERIOD),
+                  K_MSEC(CONFIG_INPUT_FT5336_PERIOD));
 #endif
-	return 0;
+    return 0;
 }
 
 #define FT5336_INIT(index)                                                     \
